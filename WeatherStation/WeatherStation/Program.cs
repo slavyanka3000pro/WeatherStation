aa33473e156c5aca40134beb89e8d3a75cf258af
uczen <uczen@STANISLAW>
2025-11-28 14:57:48 +0100

    commit

---
+++ WeatherStation/WeatherStation/Program.cs
@@ -0,0 +1,13 @@
+﻿using Database;
+
+namespace WeatherStation;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        User user1 = new User("Ja", 999);
+        User user2 = new User("Ja", 999);
+        Sensor sensor = new Sensor();
+    }
+}
