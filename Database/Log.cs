45c2945428d2fd9eae798b4fb8769644b0d2788a
uczen <uczen@OLEKSANDR>
2026-04-10 14:51:31 +0200

    commit

---
+++ Database/Log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+
+        public Log(int id, string message)
+        {
+            Id=id;
+            Message=message;
+        }
+    }
+}
