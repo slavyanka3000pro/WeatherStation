1bcbb55a41eb304a97366ed064b8b7a8862da3c3
uczen <uczen@BARTOLOMEJ>
2026-04-24 15:44:19 +0200

    test commit

---
+++ WeatherStation/UnitTests/UserRepositoryTests/GetUserTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.UserRepositoryTests
+{
+    public class GetUserTests
+    {
+        [Fact]
+        public void GetUser_ReturnsUserWithSuccess_WhenUserExistsInDatabase() {
+            var options = new DbContextOptionsBu();
+        }
+        [Fact]
+        public void GetUser_ReturnsNull_WhenUserDoesNotExistInDatabase()
+        {
+            
+        }
+    }
+}
